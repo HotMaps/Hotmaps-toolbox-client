613f1f3975342c122ccd54e8c79b631b86948ae2
lesly houndole <lesly.houndole@crem.ch>
2018-05-02 17:15:40 +0200

    add new version

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -7407,10 +7407,10 @@ var MONTHNAME = [
 // Improvement of coding style :
 // leaving one empty line between third party imports and application imports
 // listing import lines alphabetized by the module
-var prodUrl = 'http://localhost:9006/api';
+var prodUrl = 'http://hotmaps.hevs.ch:9006/api';
 var devUrl = 'http://hotmapsdev.hevs.ch:9006/api';
 var localApiUrl = 'http://localhost:5000/api';
-var geoserverProdUrl = 'http://localhost:9009/geoserver/hotmaps/wms';
+var geoserverProdUrl = 'http://hotmaps.hevs.ch:9009/geoserver/hotmaps/wms';
 var geoserverDevUrl = 'http://hotmapsdev.hevs.ch:9009/geoserver/hotmaps/wms';
 var geocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?address='; // prefer
 // prefer
