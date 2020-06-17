165edc1d05b349625f1b68abfd6be4cb84b0c806
Daniel <authentication.dh@gmail.com>
2020-06-17 18:19:00 +0200

    fix tests

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+if (!window["env"]) {
+  window["env"] = window["env"] || {};
+
+  // Default environment variables (for tests)
+  window["env"]["apiUrl"] = "http://localhost:5000/api";
+  window["env"]["geoserverUrl"] = "https://geoserver.hotmapsdev.hevs.ch/geoserver/hotmaps/wms";
+  window["env"]["wikiUrl"] = "https://wiki.hotmaps.eu";
+  window["env"]["debug"] = false;
+}
+
+
 export const environment = {
   production: false,
   apiUrl: window["env"]["apiUrl"] || "http://localhost:5000/api",
