b759f2db043d8ac2994e300f9f6c14c33917c5f1
Jeremie <jeremie.vianin@hevs.ch>
2020-06-26 16:39:09 +0200

    First draft

--- src/app/features/selection-tools/component/selection-tool.component.ts
+++ src/app/features/selection-tools/component/selection-tool.component.ts
@@ -1,6 +1,6 @@
 /**
- * Created by Dany on 20.12.17.
- */
+* Created by Dany on 20.12.17.
+*/
 
 import { Component, OnInit, Input, trigger, state, style, transition, animate } from '@angular/core';
 
@@ -10,58 +10,63 @@ import { stButtons, defaultElementSelected } from 'app/features/selection-tools/
 import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
 import { Subscription } from 'rxjs/Subscription';
 import {Logger} from '../../../shared/services/logger.service';
+import { ok } from 'assert';
+import { SelectionToolService } from '../service/selection-tool.service';
+import { map } from 'jquery';
 
 @Component({
   selector: 'htm-selection-tool',
   templateUrl: './selection-tool.component.html',
   styleUrls: ['./selection-tool.component.css'],
   animations: [
-        trigger('containerTrigger', [
-            transition(":enter", [
-              style({ opacity: 0 }),
-              animate(250, style({ opacity: 1 }))
-            ]),
-            transition(":leave", [
-              animate(250, style({ opacity: 0 }))
-            ])
-        ]),
-        trigger('elementsTrigger', [
-            state('in', style({ opacity: '1' })),
-            transition('void => *', [style({ opacity: '0' }),
-            animate('100ms 200ms')
-            ]),
-            transition('* => void', [
-                animate('50ms', style({ opacity: '0' }))
-            ])
-        ])]
+    trigger('containerTrigger', [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate(250, style({ opacity: 1 }))
+      ]),
+      transition(":leave", [
+        animate(250, style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('elementsTrigger', [
+      state('in', style({ opacity: '1' })),
+      transition('void => *', [style({ opacity: '0' }),
+      animate('100ms 200ms')
+    ]),
+    transition('* => void', [
+      animate('50ms', style({ opacity: '0' }))
+    ])
+  ])]
 })
 export class SelectionToolComponent implements OnInit, OnDestroy {
   nbElementsSelected = 0;
+  fileToUpload: File = null;
+  isHectarSelected = false;
   private scaleSelected: any;
   private subscription: Subscription;
   private subscriptionNbNutsSelected: Subscription;
-  private isHectarSelected = false;
   private isLoaBtnDisabled = true;
   private isClearBtnDisabled = true;
   private stButtons = stButtons;
   private layerSelected;
   private elementSelected = defaultElementSelected;
-  private maxSurfaceValueCM = maxSurfaceValueCM
+  private maxSurfaceValueCM = maxSurfaceValueCM;
   @Input() selectionSurface;
-  constructor(private mapService: MapService, private logger: Logger, private helper: Helper) {}
-
+  constructor(private mapService: MapService, private logger: Logger, private helper: Helper, private slcToolsService : SelectionToolService) {}
+  
   ngOnInit() {
     this.subscribeMapService();
     this.scaleSelected = this.mapService.getScaleValue();
+    this.isHectarSelected = (this.scaleSelected == hectare);
   }
   ngOnDestroy() {
     this.logger.log('SelectionToolComponent/ngOnDestroy');
     this.subscription.unsubscribe();
     this.subscriptionNbNutsSelected.unsubscribe();
-
+    
   }
   subscribeMapService() {
-
+    
     if (!this.helper.isNullOrUndefined(this.mapService.getNutsSelectedSubject())) {
       this.subscriptionNbNutsSelected = this.mapService.getNutsSelectedSubject().subscribe((value) => {
         this.nbElementsSelected = value;
@@ -71,11 +76,12 @@ export class SelectionToolComponent implements OnInit, OnDestroy {
       this.subscription = this.mapService.getScaleValueSubject().subscribe((value) => {
         this.scaleSelected = value;
         if (value === hectare) {
+          this.isHectarSelected = true;
           this.elementSelected = 'Zones selected'
         } else {
+          this.isHectarSelected = false;
           this.elementSelected = defaultElementSelected;
         }
-
       })
     }
     if (!this.helper.isNullOrUndefined(this.mapService.getNbOfLayersSelected())) {
@@ -89,15 +95,15 @@ export class SelectionToolComponent implements OnInit, OnDestroy {
         this.logger.log('nbElementsSelected =' + value )
         this.nbElementsSelected = value;
       })
-
+      
     }
     // subscribing to click event subject of MapService
     if (!this.helper.isNullOrUndefined(this.mapService.clickEventSubjectObs)) {
-        this.mapService.clickEventSubjectObs.subscribe(() => {
-          this.cursorClick(); // call cursor click method when we click anywhere in the map
-        });
+      this.mapService.clickEventSubjectObs.subscribe(() => {
+        this.cursorClick(); // call cursor click method when we click anywhere in the map
+      });
     }
-
+    
     if (!this.helper.isNullOrUndefined(this.mapService.drawCreatedSubjectObs)) {
       this.mapService.drawCreatedSubjectObs.subscribe(() => {
         this.cursorClick();
@@ -108,22 +114,23 @@ export class SelectionToolComponent implements OnInit, OnDestroy {
         this.isClearBtnDisabled = !value;
       })
     }
-
+    
     if (!this.helper.isNullOrUndefined(this.mapService.getLoadResultbuttonState())) {
       this.mapService.getLoadResultbuttonState().subscribe((value) => {
         this.isLoaBtnDisabled = !value;
       })
     }
   }
-
+  
   cursorClick() {
     const map = this.mapService.getMap();
     this.mapService.clickSelection(map);
     this.stButtons[0].isChecked = true;
   }
+  
   /**
-   * Draw method of the activated selection tool
-   */
+  * Draw method of the activated selection tool
+  */
   drawTool(button: any) {
     if (button.type === 'click') {
       this.cursorClick()
@@ -133,26 +140,58 @@ export class SelectionToolComponent implements OnInit, OnDestroy {
       this.stButtons[0].isChecked = false;
     }
   }
-
+  
   /**
-   * Load the results of the selection
-   */
+  *  Draw method when someone upload a Shapes file
+  */
+  uploadShapes(files: FileList) {
+    this.fileToUpload = files.item(0);
+    
+    // Read file
+    const fileReader = new FileReader();
+    let fileContent: any;
+    let featuresPoly = [];
+
+    fileReader.readAsText(this.fileToUpload, 'UTF-8');
+    fileReader.onload = () => {
+      fileContent = JSON.parse(<string> fileReader.result);
+      const map = this.mapService.getMap();
+      const features = fileContent.features;
+      features.forEach(feature => {
+        const geom = feature.geometry;
+        if (geom.type == "MultiPolygon") {
+          geom.coordinates.forEach(coord => {
+            const poly = L.polygon(L.GeoJSON.coordsToLatLngs(coord[0]));
+            featuresPoly.push(poly.toGeoJSON());
+          });
+        }
+      });
+      this.slcToolsService.drawShapeFromFile(map, featuresPoly);
+    }
+    fileReader.onerror = (error) => {
+      console.log(error);
+    }
+  }
+  
+  /**
+  * Load the results of the selection
+  */
   loadResultsButton() {
     const map = this.mapService.getMap();
     this.mapService.loadResultNuts(map);
   }
-
+  
   /**
-   * Clear all informations in the info box
-   */
+  * Clear all informations in the info box
+  */
   clearAllButton() {
     const map = this.mapService.getMap();
     this.mapService.clearAll(map);
     this.cursorClick();
   }
-
+  
   setClearButtonText() {
-
+    
     let zoneString = ' zones';
     if (this.layerSelected === 1 || this.nbElementsSelected === 1) {
       zoneString = ' zone';
--- src/app/shared/services/snapshot.service.ts
+++ src/app/shared/services/snapshot.service.ts
@@ -71,7 +71,6 @@ export class SnapshotService {
       name: name,
       description: description,
       date: new Date(),
-
       scale: scale,
       zones: scale !== hectare ? this.slcToolsService.nutsIdsSubject.getValue()
         : this.slcToolsService.areasSubject.getValue().map(area => {
@@ -167,20 +166,34 @@ export class SnapshotService {
           });
 
       } else {
+        console.log(snapshot.zones);
         snapshot.zones.forEach(zone => {
           let shape: any = L.geoJSON(zone as any, {
             pointToLayer: (feature: any, latlng: L.LatLng) => {
               if (feature.properties.radius) return new L.Circle(latlng, feature.properties.radius);
             }
           });
+          console.log(zone);
 
-          if (zone.properties.radius) {
+          // if("type" in zone) {
+          //   if(zone.type == "FeatureCollection") {
+          //     zone = zone.features;
+          //   } else {
+
+          //   } 
+          // } else {
+          //   console.log("erreur avec geojson ----------------");
+          // }
+          // if (zone instanceof Array) {
+
+          // }
+          
+          if (zone.properties && zone.properties.radius) {
             shape.radius = zone.properties.radius;
             shape.latLng = L.GeoJSON.coordsToLatLng(zone.geometry.coordinates);
           } else
             shape.latLngs = L.GeoJSON.coordsToLatLngs(zone.geometry.coordinates[0]);
-
-          this.slcToolsService.drawHectaresLoadingResult(map, shape);
+            this.slcToolsService.drawHectaresLoadingResult(map, shape);
         });
         if (callback) callback();
       }
