535b2d1a5275e4534b4a00d47e8bd2ffadb8f772
JMA <jean-marie.alder@hevs.ch>
2021-09-15 16:51:20 +0200

    bump from angular 4.1 to 5.2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -76,3 +76,6 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  You can import the Object.assign polyfill by MDN with npm. (mdn-polyfills)
  */
 import 'mdn-polyfills/Object.assign';
+
+// Add global to window, assigning the value of window itself.
+(window as any).global = window;
